cbf149e48a190bb6548c913cfb876364a7f52c75
zulfikar fajri <fajrizulfikar85@gmail.com>
2019-12-22 21:08:36 +0700

    create recursive version of insertion sort

--- insertionSort.js
+++ insertionSort.js
@@ -1,4 +1,4 @@
-insertionSort = (array) => {
+insertionSort = array => {
   for (outer = 1; outer < array.length; outer++) {
     for (inner = 0; inner < outer; inner++) {
       console.log(array.join(' '))
@@ -11,5 +11,29 @@ insertionSort = (array) => {
   console.log(array.join(' '))
   return array
 }
+const numbers = [8, 5, 6, 9, 3, 1, 4, 2, 7, 10]
+insertionSort(numbers)
+
+// recursive version
+let insertionSort = (arr, n) => {
+  if (n <= 1) {
+    return arr
+  }
+
+  insertionSort(arr, n - 1)
+
+  let key = arr[n - 1]
+  let j = n - 2
+
+  while (j >= 0 && arr[j] > i) {
+    arr[j + 1] = arr[j]
+    j--
+  }
+
+  arr[j + 1] = key
+
+  return arr
+}
+
 const numbers = [8, 5, 6, 9, 3, 1, 4, 2, 7, 10]
 insertionSort(numbers)
